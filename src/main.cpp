1ad631a523d23787aa46518500fd9ab13ea8f353
RuoyuLiOT-DE <ruoyu.li@origi-tech.com>
2021-09-20 21:43:25 +0200

    changed

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,8 @@ int main(int argc, char** argv) {
     Eigen::Matrix4d m = Eigen::Matrix4d::Random();
     std::cout << "Hello Eigen3 \n";
     std::cout << m << std::endl;
+    double i,j;
+    double max_coeff = m.maxCoeff(&i, &j);
+    std::cout << "Maxcoeff: " << max_coeff << " is at row: " << i << " and column: " << j << std::endl;
     return 0;
 }
